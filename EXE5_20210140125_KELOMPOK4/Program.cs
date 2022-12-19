c5f11a59475de4dd44a651d42afd7c79e803508e
riccoarisdy <riccoarisdy@gmail.com>
2022-12-19 15:51:40 +0700

    Add project files.

---
+++ EXE5_20210140125_KELOMPOK4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXE5_20210140125_KELOMPOK4
+{
+    class Program
+    {
+        int ricco, arisdy, max = 5;
+        int[] queques_array = new int[5];
+        static void Main(string[] args)
+        {
+        }
+    }
+}
